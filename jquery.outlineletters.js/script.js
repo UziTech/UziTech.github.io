71e692b209327a1dfb7d66c95c8b839c139d2387
UziTech <tbrix13+github.com@gmail.com>
2014-10-16 14:51:00 -0500

    update outline letters

--- jquery.outlineletters.js/script.js
+++ jquery.outlineletters.js/script.js
@@ -3,6 +3,13 @@ $(document).on("flatdoc:ready", function(){
 	$("#example2").text("Example 2").css({color: "#f00"}).outlineLetters({color: '#0ff'});
 	$("#example3").text("Example 3").css({color: "#0f0"}).outlineLetters({size: 5});
 	$("#example4").text("Example 4").css({color: "#f00"}).outlineLetters({color: '#0ff', size: 2});
-	$("#example5").text("Example 5").css({color: "#0f0", left: 40}).outlineLetters({size: 5, useTextShadow: false});
+	$("#example5").text("Example 5").css({color: "#0f0"}).outlineLetters({size: 5, useTextShadow: false});
 	$("#example6").text("Example 6").css({color: "#0f0"}).outlineLetters({size: 5, round: false});
+	$(window).resize(function(){
+		if($("#example1").css("float") === "none"){
+			$("#example5").css({left: 0});
+		} else {
+			$("#example5").css({left: 40});
+		}
+	}).resize();
 });
