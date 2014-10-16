e2c19c625a089b53f66bb16ac20d1735c5e69d7b
UziTech <tbrix13+github.com@gmail.com>
2014-10-16 15:03:05 -0500

    update outline letters

--- jquery.outlineletters.js/script.js
+++ jquery.outlineletters.js/script.js
@@ -1,10 +1,10 @@
 $(document).on("flatdoc:ready", function(){
-	$("#example1").text("Example 1").css({color: "#00f"}).outlineLetters();
-	$("#example2").text("Example 2").css({color: "#f00"}).outlineLetters({color: '#0ff'});
-	$("#example3").text("Example 3").css({color: "#0f0"}).outlineLetters({size: 5});
-	$("#example4").text("Example 4").css({color: "#f00"}).outlineLetters({color: '#0ff', size: 2});
-	$("#example5").text("Example 5").css({color: "#0f0"}).outlineLetters({size: 5, useTextShadow: false});
-	$("#example6").text("Example 6").css({color: "#0f0"}).outlineLetters({size: 5, round: false});
+	$("#example1").html("Example 1").css({color: "#00f"}).outlineLetters();
+	$("#example2").html("Example 2").css({color: "#f00"}).outlineLetters({color: '#0ff'});
+	$("#example3").html("Example 3").css({color: "#0f0"}).outlineLetters({size: 5});
+	$("#example4").html("Example 4").css({color: "#f00"}).outlineLetters({color: '#0ff', size: 2});
+	$("#example5").html("Example 5").css({color: "#0f0"}).outlineLetters({size: 5, useTextShadow: false});
+	$("#example6").html("Example 6").css({color: "#0f0"}).outlineLetters({size: 5, round: false});
 	$(window).resize(function(){
 		if($("#example1").css("float") === "none"){
 			$("#example5").css({left: 0});
