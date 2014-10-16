71e4e7d76e644db3da404831ce279cc2b8a590d7
UziTech <tbrix13+github.com@gmail.com>
2014-10-16 13:59:48 -0500

    fix color

--- jquery.outlineletters.js/script.js
+++ jquery.outlineletters.js/script.js
@@ -1,8 +1,8 @@
 $(document).on("flatdoc:ready", function(){
-	$("#example1").text("Example 1").outlineLetters();
-	$("#example2").text("Example 2").outlineLetters({color: '#0ff'});
-	$("#example3").text("Example 3").outlineLetters({size: 5});
-	$("#example4").text("Example 4").outlineLetters({color: '#0ff', size: 2});
-	$("#example5").text("Example 5").outlineLetters({size: 5, useTextShadow: false});
-	$("#example6").text("Example 6").outlineLetters({size: 5, round: false});
+	$("#example1").text("Example 1").css({color: "#00f"}).outlineLetters();
+	$("#example2").text("Example 2").css({color: "#f00"}).outlineLetters({color: '#0ff'});
+	$("#example3").text("Example 3").css({color: "#0f0"}).outlineLetters({size: 5});
+	$("#example4").text("Example 4").css({color: "#f00"}).outlineLetters({color: '#0ff', size: 2});
+	$("#example5").text("Example 5").css({color: "#0f0"}).outlineLetters({size: 5, useTextShadow: false});
+	$("#example6").text("Example 6").css({color: "#0f0"}).outlineLetters({size: 5, round: false});
 });
